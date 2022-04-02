948fefe3de01c967993b545fcfc606376d7ff4be
VariantT505 <thomas.dreidemy@gmail.com>
2022-04-02 16:18:10 +0200

    ajout symfony

--- public/index.php
+++ public/index.php
@@ -1,15 +1,9 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>Hypnos, l'excellence de la romance</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Site officiel du groupe hôtelier HYPNOS">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <p>Hello World !</p>
-    </body>
-</html>
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
