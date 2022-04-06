7d667e896acf0fafa49dfcaa87887ae0f8e2c74c
VariantT505 <thomas.dreidemy@gmail.com>
2022-04-06 18:37:11 +0200

    ajout footer et boucles etablissements

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Etablissements;
+use App\Entity\Suites;
 use App\Repository\EtablRepo;
+use App\Repository\SuitesRepo;
 
 class HomeController extends AbstractController
 {
@@ -21,11 +23,25 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function etablissement(EtablRepo $etablRepo): Response
     {
-        $etablissements=$etablRepo->findAll();
+        $etablissements = $etablRepo->findAll();
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'etablissements' => $etablissements,
+        ]);
+    }
+
+    #[Route('/etablissement/{id}', name: 'etab_list', methods: ['GET'])]
+    public function show(Etablissements $etablissements, SuitesRepo $suitesRepo): Response
+    {
+        $id =  $etablissements->getEtaid();
+
+        $suites = $suitesRepo->findByetaid($id);
+
+
+        return $this->render('etablissement/listing.html.twig', [
+            'etablissement' => $etablissements,
+            'suites' => $suites,
         ]);
     }
 }
