ca6391ed774a9500d7a4a23fd4ed8f911f9d21b3
VariantT505 <thomas.dreidemy@gmail.com>
2022-04-07 16:26:37 +0200

    config easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,20 +5,23 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+    }
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $url = $this->adminUrlGenerator->setController(AdministrateursCrudController::class)
+            ->generateUrl();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        return $this->redirect($url);
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
